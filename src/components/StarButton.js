9aeee76e4a7ee983a82ea828f494bb04ccfd61f1
heshamMohsen <heshammohsen96@gmail.com>
2021-01-25 19:18:45 +0200

    Updates

--- src/components/StarButton.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const StarButton = React.memo(({ repo, starred, starRepoHandler }) => {
-  return (
-    <button className="btn btn-secondary" onClick={() => starRepoHandler(repo, starred)}>
-      {starred ? "starred" : "star"}
-    </button>
-  );
-});
--- src/constants/API.js
+++
@@ -1,4 +0,0 @@
-import moment from "moment";
-
-const lastWeek = moment().subtract(7, "d").format("YYYY-MM-DD");
-export const API = `https://api.github.com/search/repositories?q=created:>${lastWeek}&sort=stars&order=desc`;
