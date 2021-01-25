46b10392508fb1ed668f4f5ed253de9c6923d040
heshamMohsen <heshammohsen96@gmail.com>
2021-01-25 17:26:04 +0200

    fix date bug

--- src/constants/API.js
+++ src/constants/API.js
@@ -1,6 +1,4 @@
-const fullYear = new Date().getFullYear();
-const month = new Date().getMonth() < 10 ? `0${new Date().getMonth()}` : new Date().getMonth();
-const day = new Date().getDate() - 7 < 10 ? `0${new Date().getDate() - 7}` : new Date().getDate();
-const lastWeek = `${fullYear}-${month}-${day}`;
+import moment from "moment";
 
+const lastWeek = moment().subtract(7, "d").format("YYYY-MM-DD");
 export const API = `https://api.github.com/search/repositories?q=created:>${lastWeek}&sort=stars&order=desc`;
